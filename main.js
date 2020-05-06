f08bf59685d5d8eef1d9e1288a0a3ca682b4e504
112batman <53634417+112batman@users.noreply.github.com>
2020-05-06 13:54:55 +0200

    Added help menu item

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.*/
 
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, MenuItem} = require('electron')
 const path = require('path')
 
 var win
@@ -18,9 +18,26 @@ function CreateWindow() {
     })
 
     win.loadFile('./views/Index2.html')
-
-    win.removeMenu()
 }
+
+var menu = new Menu()
+var help = new MenuItem({
+    click: function() {
+        const path = require('path')
+
+        var win = new BrowserWindow({
+            width: 800,
+            height: 600,
+            icon: path.join(__dirname, 'assets', 'icon-no-bg.ico')
+        })
+    
+        win.loadURL('https://112batman.gitbook.io/energiedata/')
+    },
+    label: 'Help'
+})
+
+Menu.setApplicationMenu(menu)
+menu.append(help)
 
 app.whenReady().then(() => {
     CreateWindow()
